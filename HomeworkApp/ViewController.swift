688940e373282a0d424494297e36705d9ef44975
rayz2007 <rayz2007@uw.edu>
2018-12-08 13:50:57 -0800

    made app

---
+++ HomeworkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkApp
+//
+//  Created by Ray Zhang on 12/8/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
