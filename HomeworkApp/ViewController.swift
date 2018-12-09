f3e070ab87e3c07076c8c54511ffa85d052e8289
iGuest <rayz2007@uw.edu>
2018-12-08 16:59:40 -0800

    added save image

--- HomeworkApp/ViewController.swift
+++ HomeworkApp/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Do any additional setup after loading the view, typically from a nib.
         imagePicker.delegate = self
     }
+    enum ImageSource {
+        case photoLibrary
+        case camera
+    }
+
+    @IBOutlet weak var imageTake: UIImageView!
     
     @IBAction func selectPhoto(_ sender: UIButton) {
         imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
@@ -25,7 +31,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func openCamera(_ sender: UIButton) {
-        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            selectImageFrom(.photoLibrary)
+            return
+        }
+        selectImageFrom(.camera)
+        /*if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
         {
             imagePicker.sourceType = UIImagePickerController.SourceType.camera
             imagePicker.allowsEditing = true
@@ -36,7 +47,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
+        } */
+    }
+    func selectImageFrom(_ source: ImageSource){
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+            imagePicker.sourceType = .photoLibrary
         }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: AnyObject) {
+        guard let selectedImage = imageTake.image else {
+            print("Image not found!")
+            return
+        }
+       UIImageWriteToSavedPhotosAlbum(selectedImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            showAlertWith(title: "Save error", message: error.localizedDescription)
+        } else {
+            showAlertWith(title: "Saved!", message: "Your image has been saved to your photos.")
+        }
+    }
+    func showAlertWith(title: String, message: String){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -52,9 +95,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             //let imageData = NSData(contentsOfFile: localPath!)!
             let photoURL = URL.init(fileURLWithPath: localPath!)//NSURL(fileURLWithPath: localPath!)
             print(photoURL)
-
         }
         self.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("Image not found!")
+            return
+        }
+        imageTake.image = selectedImage
     }
 
 }
