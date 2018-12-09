6cec95a4f8df624e82193bb14ef8621254c78fa7
iGuest <nb1@uw.edu>
2018-12-09 15:36:57 -0800

    made results storyboard

---
+++ HomeworkApp/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  HomeworkApp
+//
+//  Created by iGuest on 12/9/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
