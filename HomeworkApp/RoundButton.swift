3bd39b52075d90722284744c74c0a4ad909efb0b
iGuest <nb1@uw.edu>
2018-12-09 23:28:23 -0800

    button class

---
+++ HomeworkApp/RoundButton.swift
@@ -0,0 +1,37 @@
+//
+//  RoundButton.swift
+//  HomeworkApp
+//
+//  Created by iGuest on 12/9/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
