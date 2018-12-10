058a2bf8f04b1d265e646ecf1f3673cbc70abeef
jes4u <jes4u@uw.edu>
2018-12-09 18:12:41 -0800

    pulling

--- HomeworkApp/History.swift
+++ HomeworkApp/History.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
-class History: UIViewController {
+class History: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    var appdata = AppData.shared
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appdata.equationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "NameTableIdentifier")
+        if cell == nil {
+            NSLog("Creating new UITableViewCell")
+            cell = UITableViewCell(style: .value1, reuseIdentifier: "NameTableIdentifier")
+        }
+//        cell?.textLabel?.text = appdata.dictionary[indexPath.row].title
+//        cell?.detailTextLabel?.text = appdata.dictionary[indexPath.row].desc
+//        cell?.imageView?.image = UIImage(named: appdata.img[indexPath.row])
 
+        return cell!
+    }
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var tableView: UITableView!
+    
 
 }
